aa79b65a9519385fa34221a3fb4c408f2c6904bd
Azmina Karukappadath <azmina.karukappadath@tufts.edu>
2018-02-26 18:39:47 -0500

    Login things

--- src/HPRT/HPRT/middleware.py
+++
@@ -1,5 +0,0 @@
-class AuthRequiredMiddleware(object):
-    def process_request(self, request):
-        if not request.user.is_authenticated():
-            return HttpResponseRedirect(reverse('login')) # or http response
-        return None
