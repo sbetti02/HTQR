2a1eef3f7cfa53cc3837a69e3766fa641eed67ce
Scott Bettigole <scott.bettigole@tufts.edu>
2019-03-17 01:11:56 -0400

    Attempt to work with static files remotely via whitenoise

--- src/HPRT/HPRT/settings.py
+++ src/HPRT/HPRT/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.Doctor'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -162,7 +163,9 @@ TWILIO_AUTH_TOKEN = twilio_auth_token
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = 'HTQR/src/HPRT/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
