4d20b3acc1f711200914bd718b4d6aaa7d73f996
Scott Bettigole <scott.bettigole@tufts.edu>
2020-05-02 14:12:25 -0400

    Remove lat/long from sites and tables about relatives

--- src/HPRT/PatientPortal/models.py
+++ src/HPRT/PatientPortal/models.py
@@ -9,24 +9,30 @@ import datetime
 #########
 
 class Site(models.Model):
-    name = models.CharField(max_length=200, null = True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null = True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null = True)
+    name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 class Patient(models.Model):
     name = models.CharField(max_length=200)
-    gender = models.CharField(max_length=6, default=None, choices=[('',"Choose one"),('Male','Male'),('Female','Female'),('Other','Other')])
+    gender = models.CharField(
+        max_length=6,
+        default=None,
+        choices=[('', "Choose one"),
+                 ('Male', 'Male'),
+                 ('Female', 'Female'),
+                 ('Other', 'Other')])
     DOB = models.DateField(null=True)
-    blood_type = models.CharField(max_length = 10, null=True)
-    height = models.DecimalField(max_digits = 4, decimal_places = 1, null=True) # In centimeters
-    weight = models.DecimalField(max_digits = 4, decimal_places = 1, null=True) # In kg, max_digits non-inclusive
-    site = models.ForeignKey(Site, on_delete = models.CASCADE, null=True)
-    allergies = models.TextField(default='', blank = True, null = True)
-    current_medications = models.TextField(default='', blank = True, null = True)
-    phone_number = models.CharField(max_length = 10)
+    blood_type = models.CharField(max_length=10, null=True)
+    height = models.DecimalField(
+        max_digits=4, decimal_places=1, null=True)  # In centimeters
+    weight = models.DecimalField(
+        max_digits=4, decimal_places=1, null=True)  # In kg, max_digits non-inclusive
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
+    allergies = models.TextField(default='', blank=True, null=True)
+    current_medications = models.TextField(default='', blank=True, null=True)
+    phone_number = models.CharField(max_length=10)
     email = models.EmailField()
 
     def __str__(self):
@@ -38,30 +44,10 @@ class Patient(models.Model):
 
 class DocPat(models.Model):
     class Meta:
-        unique_together = (('doctor', 'patient'),) # TODO: Just make doctor, patient the PK of DocPat
+        unique_together = (('doctor', 'patient'),)
 
-    doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE, null=True)
-    patient = models.ForeignKey(Patient, on_delete = models.CASCADE, null=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.doctor.__str__() + ": " + self.patient.name
-
-
-# TODO: Does this still implicitly create an ID column? I don't want it to
-class RelativeRelationships(models.Model):
-    relationship_num = models.PositiveSmallIntegerField(primary_key=True)
-    relationship_type = models.CharField(max_length=30, unique=True)
-
-    def __str__(self):
-        return str(self.relationship_num) + " - " + self.relationship_type
-
-
-# TODO: On add/alter, add/alter reverse relationship as well
-class Relatives(models.Model):
-    person = models.ForeignKey(Patient, related_name="person1", on_delete=models.CASCADE)
-    related_to = models.ForeignKey(Patient, related_name="person2", on_delete=models.CASCADE)
-    relation = models.ForeignKey(RelativeRelationships, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.person.name + " => " + self.related_to.name + ", " + self.relation.relationship_type
-
